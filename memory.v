9ef7b2be046c7d59b1a9112b83479d8519a55409
Wallace Benites <148720798+wallaceeBenites@users.noreply.github.com>
2025-07-23 02:54:55 -0300

    teste ok

--- memory.v
+++ memory.v
@@ -4,15 +4,37 @@ module Memory #(
 )(
     input  wire        clk,
 
-    input  wire        rd_en_i,    // Indica uma solicitação de leitura
-    input  wire        wr_en_i,    // Indica uma solicitação de escrita
+    input  wire        rd_en_i,
+    input  wire        wr_en_i,
 
-    input  wire [31:0] addr_i,     // Endereço
-    input  wire [31:0] data_i,     // Dados de entrada (para escrita)
-    output wire [31:0] data_o,     // Dados de saída (para leitura)
+    input  wire [31:0] addr_i,
+    input  wire [31:0] data_i,
+    output reg  [31:0] data_o,
 
-    output wire        ack_o       // Confirmação da transação
+    output wire        ack_o
 );
 
+    reg [31:0] mem [0:MEMORY_SIZE-1];
+    initial begin
+        if (MEMORY_FILE != "") begin
+            $readmemh(MEMORY_FILE, mem);
+        end
+    end
+
+    always @(posedge clk) begin
+        if (wr_en_i) begin
+            mem[addr_i[31:2]] <= data_i;
+        end
+    end
+
+    always @(posedge clk) begin
+        if (rd_en_i) begin
+            data_o <= mem[addr_i[31:2]];
+        end else begin
+            data_o <= 32'd0;
+        end
+    end
+
+    assign ack_o = rd_en_i || wr_en_i;
 
 endmodule
--- registers.v
+++ registers.v
@@ -1,15 +1,24 @@
 module Registers (
-    input  wire clk,
-    input  wire wr_en_i,
-    
-    input  wire [4:0] RS1_ADDR_i,
-    input  wire [4:0] RS2_ADDR_i,
-    input  wire [4:0] RD_ADDR_i,
+    input  wire        clk,
+    input  wire        wr_en_i,
+
+    input  wire [4:0]  RS1_ADDR_i,
+    input  wire [4:0]  RS2_ADDR_i,
+    input  wire [4:0]  RD_ADDR_i,
 
     input  wire [31:0] data_i,
     output wire [31:0] RS1_data_o,
     output wire [31:0] RS2_data_o
 );
 
+    reg [31:0] regs [31:0];
+    assign RS1_data_o = (RS1_ADDR_i == 5'd0) ? 32'd0 : regs[RS1_ADDR_i];
+    assign RS2_data_o = (RS2_ADDR_i == 5'd0) ? 32'd0 : regs[RS2_ADDR_i];
+
+    always @(posedge clk) begin
+        if (wr_en_i && (RD_ADDR_i != 5'd0)) begin
+            regs[RD_ADDR_i] <= data_i;
+        end
+    end
 
 endmodule
